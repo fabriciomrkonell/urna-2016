c4bf8c594b17bc39ec6c3eabac81e3a86091c297
Fabricio Ronchi <fabricioronchii@gmail.com>
2016-08-01 20:54:40 -0300

    up

--- routes/vote.js
+++ routes/vote.js
@@ -15,6 +15,10 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/dados', function(req, res, next){
+	res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
 	res.send({
 		"podeVotar": true,
 		"data": {
